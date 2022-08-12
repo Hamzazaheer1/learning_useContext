7e7404da100a62b670f5a64405dd0a2a7c984a19
Hamza Zaheer <malikhamza1619619@gmail.com>
2022-08-13 00:52:40 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Child from "./components/Child";
+import RootContext from "./RootContext";
+
+// This object will be passed to the GrandChild component through context
+const data = {
+  number: 123,
+  text: "ABC",
+  func: () => {
+    alert("Hello!");
+  },
+};
+
+function App() {
+  return (
+    // RootContext Provider
+    <RootContext.Provider value={data}>
+      <div style={{ padding: 30 }}>
+        <Child />
+      </div>
+    </RootContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/GrandChild.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+
+import RootContext from "../RootContext";
+
+const GrandChild = () => {
+  // useContext hook
+  const data = useContext(RootContext);
+
+  return (
+    <>
+      <p>{data.number}</p>
+      <p>{data.text}</p>
+      <p>
+        <button onClick={data.func}>A Button</button>
+      </p>
+    </>
+  );
+};
+
+export default GrandChild;
